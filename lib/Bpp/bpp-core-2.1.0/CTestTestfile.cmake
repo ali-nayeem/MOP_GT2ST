9180ec047a1d91d51d9ec5db7649a5417f7dbfe0
ali_nayeem <nayeem007.gmail.com>
2019-01-21 09:11:23 +0600

    .gitignore fix

--- lib/Bpp/bpp-core-2.1.0/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ali_nayeem/Projects/MO-Phylogenetics/lib/Bpp/bpp-core-2.1.0
-# Build directory: /Users/ali_nayeem/Projects/MO-Phylogenetics/lib/Bpp/bpp-core-2.1.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- lib/Bpp/bpp-phyl-2.1.0/CMakeFiles/apidoc-stable.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/apidoc-stable"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/apidoc-stable.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
