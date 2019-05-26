eafe48740e22708b8c6dc9405fbc280c51d15858
Muhammad Ali Nayeem <nayeem007@gmail.com>
2019-05-26 20:09:16 +0600

    Add 15-taxon data

--- src/experiments/main/StandardStudyExecution.cpp
+++ src/experiments/main/StandardStudyExecution.cpp
@@ -58,7 +58,7 @@ int main(int argc, char ** argv) {
   StandardStudyExecution * exp = new StandardStudyExecution() ;
 
   // Name of the experiment:
-  exp->experimentName_ = "Nayeem18May";
+  exp->experimentName_ = "10-taxonStudy26May";
   exp->keepCheckpoint_ = true;
 
   // List of algorithm names to be used in the experiment
